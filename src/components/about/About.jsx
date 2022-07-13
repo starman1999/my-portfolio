172f50c5d1a93b5110167586a354db9dd585c542
MoonRepo <levimounir12@gmail.com>
2022-07-13 19:19:49 +0100

    navigation bar

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,7 @@ import './about.css';
 
 const About = () =>{
     return(
-        <div>About Me</div>
+        <section id='about' >About Me</section>
     )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './nav.css'
+import {AiFillHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {FaBook} from 'react-icons/fa'
+import {RiServiceLine} from 'react-icons/ri'
+import {RiMessage3Fill} from 'react-icons/ri'
+
 
 const Nav = () => {
   return (
-    <div>nav</div>
+    <nav>
+      <a href='#home' className='active'> <AiFillHome/> </a>
+      <a href='#about'> <AiOutlineUser/> </a>
+      <a href='#experience'> <FaBook/> </a>
+      <a href='#services'> <RiServiceLine/> </a>
+      <a href='#contact'> <RiMessage3Fill/> </a>
+    </nav>
   )
 }
 
